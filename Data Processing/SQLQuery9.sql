f14b0d56c06baafdfc7f9b1c21d855c048c21419
Modie <lesejane.modise@gmail.com>
2025-11-13 19:45:17 +0200

    Add files via upload

---
+++ Data Processing/SQLQuery9.sql
@@ -0,0 +1,132 @@
+SELECT
+  *
+FROM
+  "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+LIMIT
+  10;
+
+
+  -- Looking into the top selling MAKE, MODEL PER YEAR AND WHICH STATE
+SELECT MAKE, MODEL,YEAR,STATE, SUM(SELLINGPRICE) AS TOTAL_SALE
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+-- ('BMW','FORD')
+GROUP BY ALL
+ORDER BY YEAR DESC;
+
+-- Chenking which year the made the record breaking total revenue
+SELECT YEAR, SUM(SELLINGPRICE) AS TOTAL_SALE
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+GROUP BY ALL
+ORDER BY TOTAL_SALE DESC;
+
+-- Analyzing which MODEL made a staggering revenue through out
+SELECT TOP 10 MAKE,model, SELLINGPRICE, COUNT(*)
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+WHERE SELLINGPRICE IS NOT NULL
+GROUP BY ALL
+ORDER BY SELLINGPRICE DESC;
+
+-- I want to build a logic (relationship between sellingprice,odometer and year)
+SELECT SELLINGPRICE,ODOMETER,YEAR, 
+       -- AVG(year) AS avg_year,
+       -- AVG(odometer) AS avg_odometer,
+    CASE 
+        WHEN SELLINGPRICE <= 15000 AND ODOMETER < 15999 THEN  'NEW'
+        WHEN SELLINGPRICE  BETWEEN 5000 AND 12999 AND ODOMETER BETWEEN 16000 AND 50000 THEN 'FAIRLY_NEW'
+        ELSE 'Not Worth it'
+        END AS Logic
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+WHERE ODOMETER < 10000
+GROUP BY ALL;       
+
+SELECT 
+    CORR(sellingprice, year) AS price_year_correlation,
+    CORR(sellingprice, odometer) AS price_odometer_correlation,
+    CORR(year, odometer) AS year_odometer_correlation
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+WHERE odometer IS NOT NULL AND sellingprice IS NOT NULL;
+        
+
+-- Checking the avg condition
+SELECT MAKE, AVG(MODEL) AS AVG_MODEL
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+--WHERE MAKE IN ('BMW')
+GROUP BY ALL
+ORDER BY AVG_MODEL DESC;
+
+--Checking how many units sold per make
+SELECT MAKE,COUNT(MODEL) AS MODEL,SUM(SELLINGPRICE) AS TOTAL_SALE
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+GROUP BY ALL
+ORDER BY TOTAL_SALE DESC;
+
+--Checking MIN AND MAX Sellingprice
+SELECT MIN(SELLINGPRICE)
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL";
+
+--Checking MIN AND MAX ODOMETER
+SELECT MIN(odometer)
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL";
+
+--Checking Condition Max and Min
+
+SELECT MIN(CONDITION)
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL";
+
+--CHECKING THE MOST EXPENSICE MODEL
+SELECT TOP 5 SELLINGPRICE,MODEL,ODOMETER,COUNT(MODEL)
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+WHERE SELLINGPRICE IS NOT NULL
+GROUP BY ALL
+ORDER BY SELLINGPRICE DESC;
+
+--Giving Null the discription
+SELECT COALESCE(TRANSMISSION,'UNKNOWN') AS TRANSMISSION
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL";
+
+SELECT DISTINCT(STATE) AS STATE
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL";
+
+--Grouping my ratings nicely
+SELECT
+  CASE 
+        WHEN CONDITION < 16 THEN 'Good rating'
+        WHEN CONDITION BETWEEN 17 AND 33 THEN 'Average rating'
+        WHEN CONDITION IS NULL THEN 'Unknown rating'
+        ELSE 'Poor rating'
+        END AS RATING_SCALE, COUNT(RATING_SCALE) COUNT_RATINGS
+        
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+GROUP BY ALL;
+        
+
+
+
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- Final Code
+SELECT YEAR, COALESCE(MAKE,'Unknown') AS MAKE, MODEL, BODY,COALESCE(TRANSMISSION,'Unknown') AS TRANSMISSION, STATE, COUNT(*) AS Number_Of_Cars_Sold , ODOMETER, COLOR,COALESCE(MMR,0) AS MMR, COALESCE(SELLINGPRICE,0) AS SELLINGPRICE ,((SELLINGPRICE - MMR) * 100.0 / MMR) AS Profit_Margin_Percentage,SUM(SELLINGPRICE - MMR) AS Profit_Margin,SALEDATE,
+  -- LEAD(SELLINGPRICE) OVER(partition by MMR ORDER BY YEAR) AS Depreciation_Rate,
+    CASE
+        WHEN ODOMETER < 79999 THEN 'Low Millage'
+        WHEN ODOMETER BETWEEN 80000 AND 129999 THEN 'Average Millage'
+        WHEN ODOMETER IS NULL THEN 'Unknow Millage'
+        ELSE 'High Millage'
+    END AS Millage_Category,
+     CASE 
+        WHEN CONDITION < 16 THEN 'Good rating'
+        WHEN CONDITION BETWEEN 17 AND 33 THEN 'Average rating'
+        WHEN CONDITION IS NULL THEN 'Unknown rating'
+        ELSE 'Poor rating'
+        END AS RATING_SCALE, COUNT(RATING_SCALE) COUNT_RATINGS
+    
+FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
+WHERE year BETWEEN 1982 AND 2015
+    AND sellingprice IS NOT NULL 
+    AND odometer IS NOT NULL
+    AND COLOR IS NOT NULL
+GROUP BY ALL
+ORDER BY year DESC;    
+
+
