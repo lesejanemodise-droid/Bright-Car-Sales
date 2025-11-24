fa7c87cc10a7149bff3e6be71338d7735486b392
Modie <lesejane.modise@gmail.com>
2025-11-24 20:24:49 +0200

    Update SQLQuery9.sql

--- Data Processing/SQLQuery9.sql
+++ Data Processing/SQLQuery9.sql
@@ -103,30 +103,135 @@ GROUP BY ALL;
 
 
 
+SELECT MAX(CONDITION)
+  FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL";
 
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------
--- Final Code
-SELECT YEAR, COALESCE(MAKE,'Unknown') AS MAKE, MODEL, BODY,COALESCE(TRANSMISSION,'Unknown') AS TRANSMISSION, STATE, COUNT(*) AS Number_Of_Cars_Sold , ODOMETER, COLOR,COALESCE(MMR,0) AS MMR, COALESCE(SELLINGPRICE,0) AS SELLINGPRICE ,((SELLINGPRICE - MMR) * 100.0 / MMR) AS Profit_Margin_Percentage,SUM(SELLINGPRICE - MMR) AS Profit_Margin,SALEDATE,
-  -- LEAD(SELLINGPRICE) OVER(partition by MMR ORDER BY YEAR) AS Depreciation_Rate,
-    CASE
-        WHEN ODOMETER < 79999 THEN 'Low Millage'
-        WHEN ODOMETER BETWEEN 80000 AND 129999 THEN 'Average Millage'
-        WHEN ODOMETER IS NULL THEN 'Unknow Millage'
-        ELSE 'High Millage'
-    END AS Millage_Category,
-     CASE 
-        WHEN CONDITION < 16 THEN 'Good rating'
-        WHEN CONDITION BETWEEN 17 AND 33 THEN 'Average rating'
-        WHEN CONDITION IS NULL THEN 'Unknown rating'
-        ELSE 'Poor rating'
-        END AS RATING_SCALE, COUNT(RATING_SCALE) COUNT_RATINGS
-    
-FROM "BRIGHT_CAR_SALE"."CAR_SCH"."CAR_TBL"
-WHERE year BETWEEN 1982 AND 2015
-    AND sellingprice IS NOT NULL 
-    AND odometer IS NOT NULL
-    AND COLOR IS NOT NULL
-GROUP BY ALL
-ORDER BY year DESC;    
+ SELECT TRANSMISSION 
+ FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL"
+  WHERE TRANSMISSION IN ('automatic','manual');
+
+  SELECT DISTINCT(BODY)
+   FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL";
+
+  
+SELECT STATE,
+    CASE STATE
+        WHEN 'fl' THEN 'Florida'
+        WHEN 'mi' THEN 'Michigan'
+        WHEN 'on' THEN 'Ontario'
+        WHEN 'hi' THEN 'Hawaii'
+        END AS STATE
+      FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL"
+  WHERE STATE IN ('fl','mi','on','hi') AND STATE IS NOT NULL;
+
+  
+ SELECT YEAR - DATE(TRY_TO_TIMESTAMP(SALEDATE, 'DY Mon DD YYYY HH24:MI:SS')) as SALE_DATE,
+ FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL";
+
+SELECT DISTINCT(SELLER),STATE
+ FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL";
+
+  
+SELECT SELLINGPRICE,STATE,
+ RANK() OVER(PARTITION BY STATE ORDER BY SELLINGPRICE DESC) AS PRICE_RANKING
+FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL";           
+ 
+-----------------------------------------------------------------------------------------------------------------------------------
+  SELECT  YEAR AS YEAR_MANUFACTURED,
+        MAKE,
+        MODEL,
+        BODY,
+        TRANSMISSION,
+        ODOMETER AS MILLAGE,
+        COLOR,
+        SELLER,
+        COUNT(DISTINCT(VIN)) AS NUMBER_OF_CAR_SOLD,
+        COALESCE(MMR,0) AS MMR,
+        SELLINGPRICE AS PRICE,
+        SUM(SELLINGPRICE - MMR) AS Profit_Margin,
+        SALEDATE,
+        DATE(TRY_TO_TIMESTAMP(SALEDATE, 'DY Mon DD YYYY HH24:MI:SS')) as SALE_DATE,
+        TIME(TRY_TO_TIMESTAMP(SALEDATE, 'DY Mon DD YYYY HH24:MI:SS')) as SALE_TIME,
+        DAYNAME(TRY_TO_TIMESTAMP(SALEDATE, 'DY Mon DD YYYY HH24:MI:SS')) as DAY_NAME,
+              CASE 
+                   WHEN LENGTH(TRIM(STATE)) = 2 AND STATE NOT LIKE '%[0-9]%' THEN STATE
+                   ELSE 'UNKNOWN'
+        END AS STATE_Abrr,
+               CASE STATE
+        -- US States
+        WHEN 'fl' THEN 'Florida'
+        WHEN 'mi' THEN 'Michigan'
+        WHEN 'hi' THEN 'Hawaii'
+        WHEN 'ga' THEN 'Georgia'
+        WHEN 'pa' THEN 'Pennsylvania'
+        WHEN 'tx' THEN 'Texas'
+        WHEN 'il' THEN 'Illinois'
+        WHEN 'md' THEN 'Maryland'
+        WHEN 'sc' THEN 'South Carolina'
+        WHEN 'va' THEN 'Virginia'
+        WHEN 'tn' THEN 'Tennessee'
+        WHEN 'or' THEN 'Oregon'
+        WHEN 'ms' THEN 'Mississippi'
+        WHEN 'la' THEN 'Louisiana'
+        WHEN 'ok' THEN 'Oklahoma'
+        WHEN 'oh' THEN 'Ohio'
+        WHEN 'mn' THEN 'Minnesota'
+        WHEN 'al' THEN 'Alabama'
+        WHEN 'wi' THEN 'Wisconsin'
+        WHEN 'co' THEN 'Colorado'
+        WHEN 'mo' THEN 'Missouri'
+        WHEN 'ny' THEN 'New York'
+        WHEN 'wa' THEN 'Washington'
+        WHEN 'az' THEN 'Arizona'
+        WHEN 'ne' THEN 'Nebraska'
+        WHEN 'ut' THEN 'Utah'
+        WHEN 'nm' THEN 'New Mexico'
+        WHEN 'nc' THEN 'North Carolina'
+        WHEN 'nv' THEN 'Nevada'
+        WHEN 'in' THEN 'Indiana'
+        WHEN 'nj' THEN 'New Jersey'
+        WHEN 'ma' THEN 'Massachusetts'
+        WHEN 'ca' THEN 'California'
+        
+        -- Canadian Provinces
+        WHEN 'on' THEN 'Ontario'
+        WHEN 'qc' THEN 'Quebec'
+        WHEN 'ns' THEN 'Nova Scotia'
+        WHEN 'ab' THEN 'Alberta'
+        
+        -- US Territories
+        WHEN 'pr' THEN 'Puerto Rico'
+        
+        ELSE STATE 
+    END AS STATE_NAME,
+   -- RANK() OVER(PARTITION BY FULL_NAME_STATE ORDER BY SELLINGPRICE DESC) AS PRICE_RANKING,
+               CASE 
+                   WHEN CONDITION < 18 THEN 'Good rating'
+                   WHEN CONDITION BETWEEN 19 AND 38 THEN 'Average rating'
+                   ELSE 'Poor rating'
+                   END AS RATING_SCALE,   
+FROM
+  "BRIGHTCAR_SALES"."CAR_SCH"."CAR_TBL"
+  WHERE TRY_TO_TIMESTAMP(SALEDATE, 'DY Mon DD YYYY HH24:MI:SS') IS NOT NULL 
+  AND TRANSMISSION IN ('automatic','manual')
+  AND MAKE IS NOT NULL
+  AND MODEL IS NOT NULL
+  AND BODY IS NOT NULL
+  AND TRANSMISSION IS NOT NULL
+  AND ODOMETER IS NOT NULL
+  AND COLOR IS NOT NULL
+  AND CONDITION IS NOT NULL
+  AND YEAR IS NOT NULL
+  AND STATE_NAME IS NOT NULL
+  AND SELLINGPRICE IS NOT NULL
+  GROUP BY ALL;
+
 
 
